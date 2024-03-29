d3c050560ed0cec1c828ac375faedcffc9862b04
4lan Lucas <alanlucas0707@gmail.com>
2024-03-29 10:20:17 -0300

    added Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,7 +33,12 @@ public class Program {
 
         System.out.println("=== Test 4: seller insert ===");
         Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0,department);
-        sellerDao.insert(newSeller);
+        //sellerDao.insert(newSeller);
         System.out.println("Inserted! new id = " + newSeller.getId());
+
+        System.out.println("=== Test 5: seller Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
     }
 }
